dad59a6a24763290047fa84c8751a0637d98664d
Mustafa <hazim6163@gmail.com>
2019-06-10 15:56:19 +0200

    create the secret page with php functions

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>404</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet"> 
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+		    	<form action="testPass.php" method="post" id="pass" autocomplete="off">
+                      <input autocomplete="off" type="text" class="form-control inputpass" name="password">
+                </form>
+                  
+			<div class="notfound-404">
+				<h1>4<span>0</span>4</h1>
+			</div>
+			<p>The page you are looking <a href="javascript:{}" onclick="document.getElementById('pass').submit();" class="for" name="pass">for</a> might have been removed had its name changed or is temporarily unavailable.</p>
+			<br>
+			<?php
+            
+            ?>
+		</div>
+	</div>
+	
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
---
+++ testPass.php
@@ -0,0 +1,46 @@
+<?php
+if(isset($_POST['password'])){
+                $pass = $_POST['password'];
+                if($pass == 1101998){
+                    ?>
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+<?php
+                    
+    }else{
+        echo "<script>location.href='index.php';</script>";
+    }
+
+}else{
+    echo "<script>location.href='index.php';</script>";
+}
+?>
