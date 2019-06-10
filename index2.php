1a0bd9539cf7aaedf067246c665428d0f6c1c5d6
Mustafa <hazim6163@gmail.com>
2019-06-10 20:41:35 +0200

    create meassages table

--- index2.php
+++ index2.php
@@ -10,10 +10,25 @@ if(isset($_POST['password'])){
 <div class="container-fluid  h-100 wrapperSetName" style="">
     <div class="row h-100 justify-content-center " style="padding-top: 10vh">
         <div class="col-12  col-lg-6 col-xl-4">
-
-            <?php echo $username;?>
-            
-            
+            <table class="table table-borderless messageTable" id="messagesTable">
+              <thead>
+                <!--<tr>
+                  <th scope="col" class="messageTo">marha7adhjas ajksdhas jhaskjd ashdjka skjdkjasdj kjasdkj askjd a</th>
+                </tr> -->
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row" class="messageTo"></th>
+                </tr>
+                <tr>
+                  <th scope="row" class="messageFrom"></th>
+                </tr>
+              </tbody>
+            </table>
+            <script>
+                var objDiv = document.getElementById("messagesTable");
+                objDiv.scrollTop = objDiv.scrollHeight;
+            </script>
         </div>
     </div>
 </div> 
