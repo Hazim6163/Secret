6f86dcd5a404628586b3fabf37eabef73d15e7dc
Mustafa <hazim6163@gmail.com>
2019-06-11 21:11:41 +0200

    change loginToChanell.php name to right name loginFormToChanell

--- include/checkChanell.php
+++ include/checkChanell.php
@@ -8,7 +8,7 @@ if(isset($_POST['secret_chanell'])){
         session_start();
         $_SESSION['secret_chanell'] = $_POST['secret_chanell'];
         session_write_close();
-        header("Location: loginToChanell.php");
+        header("Location: loginFormToChanell.php");
     }else{
         header("Location: ../index.php");
     }
