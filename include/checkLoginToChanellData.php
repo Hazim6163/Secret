090de33883d58cf3c1dd87e9be96fa9906f6734f
Mustafa <hazim6163@gmail.com>
2019-06-11 22:56:16 +0200

    login opration

---
+++ include/checkLoginToChanellData.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+
+if(isset($_POST['secret_chanell'], $_POST['username'], $_POST['password'])){
+    if (user_inside_chanell_validate($_POST['username'], $_POST['password'], $_POST['secret_chanell'])){
+        session_start();
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['password'] = $_POST['password'];
+        session_write_close();
+        
+    }else{
+        session_start();
+        $_SESSION['secret_chanell'] = $_POST['secret_chanell'];
+        $_SESSION['login_information_error'] = true;
+        session_write_close();
+        header("Location: loginFormToChanell.php");
+    }
+    
+}else{
+    header("Location: ../index.php");
+}
+?>
