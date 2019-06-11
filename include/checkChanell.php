064bb08d696bc387114e8f42134cfeef8c12db6a
Mustafa <hazim6163@gmail.com>
2019-06-11 18:40:35 +0200

    validate secret chanell throw function

--- include/checkChanell.php
+++ include/checkChanell.php
@@ -4,34 +4,17 @@ include 'db.php';
 
 if(isset($_POST['secret_chanell'])){
     
-    $secret_chanell = $_POST['secret_chanell'];
-    $secret_chanell = mysqli_real_escape_string($connection, $secret_chanell);
-    
-    // set up the query
-    $query = "SELECT * FROM secret_chanells WHERE secret_chanell = '{$secret_chanell}' ";
-    $get_chanell_query = mysqli_query($connection, $query);
-    
-    // if there's an error :
-    if(!$get_chanell_query){
-        die("QUERY FAILED" . mysqli_error($connection));
-    }
-    
-    while($row = mysqli_fetch_array($get_chanell_query)){
-        $secret_chanell_db = $row['secret_chanell'];
-    }
-    
-    if($secret_chanell == ''){
-        header("Location: ../index.php");;
-    }else if($secret_chanell == $secret_chanell_db){
+    if (secret_chanell_validate($_POST['secret_chanell'])){
         session_start();
-        $_SESSION['secret_chanell'] = $secret_chanell;
-        header("Location: loginToChanell.php");
+        $_SESSION['secret_chanell'] = $_POST['secret_chanell'];
         session_write_close();
+        header("Location: loginToChanell.php");
     }else{
         header("Location: ../index.php");
     }
     
 }else{
-    echo "<script>location.href='http://192.168.2.108/workstation/secret/index.php';</script>";
+    header("Location: ../index.php");
 }
+
 ?>
--- include/db.php
+++ include/db.php
@@ -15,4 +15,37 @@ if(!$connection){
     echo 'not connected';
 }
 
+
+$secret_chanell_db  = null;
+$secret_chanell  = null;
+
+function secret_chanell_validate($secret_chanell){
+    global $connection;
+    $secret_chanell_db  = null;
+    
+    
+    $secret_chanell = mysqli_real_escape_string($connection, $secret_chanell);
+    
+    // set up the query
+    $query = "SELECT * FROM secret_chanells WHERE secret_chanell = '{$secret_chanell}' ";
+    $get_chanell_query = mysqli_query($connection, $query);
+    
+    // if there's an error :
+    if(!$get_chanell_query){
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+    
+    while($row = mysqli_fetch_array($get_chanell_query)){
+        $secret_chanell_db = $row['secret_chanell'];
+    }
+    
+    if($secret_chanell == ''){
+        return false;
+    }else if($secret_chanell == $secret_chanell_db){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 ?>
