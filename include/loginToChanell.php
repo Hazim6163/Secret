bfa2f4dca71b77bf1f4209debb89446d646434c8
Mustafa <hazim6163@gmail.com>
2019-06-11 20:19:51 +0200

    fix some errors inside form

--- include/loginToChanell.php
+++ include/loginToChanell.php
@@ -18,23 +18,18 @@ if(isset($_SESSION['secret_chanell'])){
 <div class="container-fluid   wrapperSetName" style="">
     <div class="row h-100 justify-content-center " style="padding-top: 10vh">
         <div class="col-12  col-lg-6 col-xl-4">
-
-            <img src="img/heart.png" class="img-fluid" alt="Heart">
-
-
-            <form action="index2.php" method="post" autocomplete="off">
-             <input type="hidden" name="password" value=<?php echo $secret_chanell;?></inpu>
-              <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" autocomplete="off" class="form-control" name="username" aria-describedby="usernameHelp" placeholder="Enter your Username" style="background-color: #36282B; border:0px; color:#d6d4d4">
-                <small id="usernameHelp" class="form-text " style="color: #954242; ">It will be your Name in the chat...</small>
-                <label for="username">Password</label>
-                <input type="password" autocomplete="off" class="form-control" name="password"  placeholder="Enter your Password" style="background-color: #36282B; border:0px; color:#d6d4d4">
-              </div>
-            <button type="submit" class="btn btn-danger" name="setChatName">Let's go</button>    
+            <img src="img/heart.png" class="img-fluid" alt="Heart"/>
+            <form action="main.php" method="post" autocomplete="off">
+                   <div class="form-group">
+                        <input type="hidden" name="secret_chanell" value=<?php echo $secret_chanell;?></input>
+                        <label for="username">Username</label>
+                        <input type="text" autocomplete="off" class="form-control" name="username" aria-describedby="usernameHelp" placeholder="Enter your Username" style="background-color: #36282B; border:0px; color:#d6d4d4"></input>
+                        <small id="usernameHelp" class="form-text " style="color: #954242; ">It will be your Name in the chat...</small>
+                        <label for="username">Password</label>
+                        <input type="password" autocomplete="off" class="form-control" name="password"  placeholder="Enter your Password" style="background-color: #36282B; border:0px; color:#d6d4d4"></input>
+                  </div>
+                 <button type="submit" class="btn btn-danger" name="login">Let's go</button>    
             </form>
-            
-            
         </div>
     </div>
 </div>
